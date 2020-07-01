ba3cffcc8d37ee12a5cd359fc74bbbee576b3a30
Dave Longley <dlongley@digitalbazaar.com>
2020-07-01 18:36:43 -0400

    Add more tests.

--- test/mocha/10-resources.js
+++ test/mocha/10-resources.js
@@ -8,7 +8,7 @@ const {resources} = require('bedrock-resource-restriction');
 const {ACQUIRER_ID, RESOURCES, assertCheckResult} = require('./helpers.js');
 
 describe('resources', function() {
-  it('should check a request', async function() {
+  it('should authorize a request with no restrictions', async function() {
     const now = Date.now();
     const acquirerId = ACQUIRER_ID;
     const request = [
@@ -16,6 +16,102 @@ describe('resources', function() {
     ];
     const acquisitionTtl = 30000;
     const result = await resources.check({acquirerId, request, acquisitionTtl});
+    const expectedResult = {
+      authorized: true,
+      excessResources: [],
+      untrackedResources: [RESOURCES.APPLE]
+    };
+    assertCheckResult(result, expectedResult);
+  });
+
+  it('should allow acquire on an unrestricted resource', async function() {
+    const now = Date.now();
+    const acquirerId = ACQUIRER_ID;
+    const request = [
+      {resource: RESOURCES.APPLE, count: 1, requested: now}
+    ];
+    const acquisitionTtl = 30000;
+    const result = await resources.acquire(
+      {acquirerId, request, acquisitionTtl});
+    const expectedResult = {
+      authorized: true,
+      excessResources: [],
+      untrackedResources: [RESOURCES.APPLE]
+    };
+    assertCheckResult(result, expectedResult);
+  });
+
+  it.skip('should authorize a request with a restriction', async function() {
+    const now = Date.now();
+    const acquirerId = ACQUIRER_ID;
+    const request = [
+      {resource: RESOURCES.APPLE, count: 1, requested: now}
+    ];
+    const acquisitionTtl = 30000;
+    const result = await resources.check({acquirerId, request, acquisitionTtl});
+    const expectedResult = {
+      authorized: true,
+      excessResources: [],
+      untrackedResources: [RESOURCES.APPLE]
+    };
+    assertCheckResult(result, expectedResult);
+  });
+
+  it.skip('should acquire a restricted resource', async function() {
+    const now = Date.now();
+    const acquirerId = ACQUIRER_ID;
+    const request = [
+      {resource: RESOURCES.APPLE, count: 1, requested: now}
+    ];
+    const acquisitionTtl = 30000;
+    const result = await resources.acquire(
+      {acquirerId, request, acquisitionTtl});
+    assertCheckResult(result);
+  });
+
+  it.skip('should deny a request with a restriction', async function() {
+    const now = Date.now();
+    const acquirerId = ACQUIRER_ID;
+    const request = [
+      {resource: RESOURCES.APPLE, count: 1, requested: now}
+    ];
+    const acquisitionTtl = 30000;
+    const result = await resources.check({acquirerId, request, acquisitionTtl});
+    assertCheckResult(result);
+  });
+
+  it.skip('should deny an acquisition with a restriction', async function() {
+    const now = Date.now();
+    const acquirerId = ACQUIRER_ID;
+    const request = [
+      {resource: RESOURCES.APPLE, count: 1, requested: now}
+    ];
+    const acquisitionTtl = 30000;
+    const result = await resources.acquire(
+      {acquirerId, request, acquisitionTtl});
+    assertCheckResult(result);
+  });
+
+  it.skip('should force-record an unauthorized acquisition', async function() {
+    const now = Date.now();
+    const acquirerId = ACQUIRER_ID;
+    const request = [
+      {resource: RESOURCES.APPLE, count: 1, requested: now}
+    ];
+    const acquisitionTtl = 30000;
+    const result = await resources.acquire(
+      {acquirerId, request, acquisitionTtl, forceAcquisition: true});
+    assertCheckResult(result);
+  });
+
+  it.skip('should release an acquired resource', async function() {
+    const acquirerId = ACQUIRER_ID;
+    const request = [
+      {resource: RESOURCES.APPLE, count: 1}
+    ];
+    const acquisitionTtl = 30000;
+    const result = await resources.release(
+      {acquirerId, request, acquisitionTtl});
     assertCheckResult(result);
   });
 });
